c07cd3f80ceb9c722f690c5ca1f1374e79976fc1
CaptainM <caroline.a.morgan@vanderbilt.edu>
2015-01-20 17:28:12 -0500

    Added models.rb for users and ingredients, including validation. Added views for the root file to get root to display. Edited routes. Migrated Ingredients and Users Added lib API files for nokogiri and yummly.

--- db/migrate/20150120210708_create_user.rb
+++ db/migrate/20150120210708_create_user.rb
@@ -1,6 +1,11 @@
 class CreateUser < ActiveRecord::Migration
   def change
     create_table :users do |t|
+    	t.string :username
+    	t.string :password_digest
+    	t.string :first_name
+
+    	t.timestamps
     end
   end
 end
---
+++ lib/yummly_api.rb
@@ -0,0 +1,5 @@
+Yummly.configure do |config|
+  config.app_id = "27a90be3"
+  config.app_key = "bc25f1525df5e8e10e9e7972c538d06a"
+  config.use_ssl = true # Default is false
+end
