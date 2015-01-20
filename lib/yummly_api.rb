ef8855444e76872b93d3db66a7ff6b93145ff5b7
CaptainM <caroline.a.morgan@vanderbilt.edu>
2015-01-20 17:33:02 -0500

    botched the API code so that I'm not pushing the API key to github

--- lib/yummly_api.rb
+++ lib/yummly_api.rb
@@ -1,5 +1,5 @@
 Yummly.configure do |config|
   config.app_id = "27a90be3"
-  config.app_key = "bc25f1525df5e8e10e9e7972c538d06a"
+  config.app_key = "bc25f1525df5e8e10e9e7972c53"
   config.use_ssl = true # Default is false
 end
