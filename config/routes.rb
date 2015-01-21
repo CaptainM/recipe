c07a27df1badebd5fe323eaef81bbb1552e68488
CaptainM <caroline.a.morgan@vanderbilt.edu>
2015-01-21 14:57:57 -0500

    Moved API code into Config, vs lib. Used ENV keys from bash profile. Added a search route and path for the fetch request. Made the 'add' button add the ingredient to the page and clear the input box. Added a template, but realized I didn't need it. Added recipe model. Added recipe collection. Added new collection object to the app.js file upon initialization.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :ingredients
 
   root to: 'application#index'
+  get '/search' => 'application#search'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- lib/yummly_api.rb
+++
@@ -1,5 +0,0 @@
-Yummly.configure do |config|
-  config.app_id = "27a90be3"
-  config.app_key = "bc25f1525df5e8e10e9e7972c53"
-  config.use_ssl = true # Default is false
-end
